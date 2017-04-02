2fe8ef96f920966be347c79b1a8ab71b3a7d50a2
Vladislav <zveronym@gmail.com>
2017-04-02 15:33:13 +0300

    Delete config.js
    тупанул)

--- app/config.js
+++
@@ -1,7 +0,0 @@
-exports.port = 8080;
-exports.secret = 'AAC secret';
-exports.dbLogin = 'admin';
-exports.dbPass = 'da-Strateg-reestr';
-exports.dbIp = '80.93.177.208:27017/ERIO'
-/*exports.dbIp = 'localhost:27017/ERIO';*/
-exports.emailPass = 'Da-Strategy';
